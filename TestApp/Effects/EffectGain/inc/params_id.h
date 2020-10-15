9247913c27c3d758b7414feeb109ceff563c3fbf
tryznav <tryznav@gmail.com>
2020-10-15 17:52:45 +0300

    all

--- TestApp/Effects/EffectGain/inc/params_id.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PARAMS_ID_H__
-#define __PARAMS_ID_H__
-
-#define PRM_GAIN_dB_ID          'Gain'
-#define PRM_SAMPLE_RATE_ID      'SMPR'
-#define PRM_FREQ_START_ID       'FSTA'
-#define PRM_FREQ_END_ID         'FEND'
-
-#endif
