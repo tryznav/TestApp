0fc71194bee5beb365c11d1b509a67aeccf3bdff
tryznav <tryznav@gmail.com>
2020-11-18 16:44:02 +0200

    rev

--- TestApp/Effects/Fxd/Chain_fxd/api/inc/chain_fxd_api.h
+++ TestApp/Effects/Fxd/Chain_fxd/api/inc/chain_fxd_api.h
@@ -12,57 +12,57 @@
 #include "eq_fxd.h"
 
 typedef struct chain_fxf_prm_s{
-    eq_prm_t        eq1;
-    int32_t         eq1_enable;
-    cross_prm_t     cross;
-    int32_t         cross_enable;
-    comp_prm_t      comp_1b;
-    int32_t         comp_1b_enable;
-    comp_prm_t      comp_2b;
-    int32_t         comp_2b_enable;
-    comp_prm_t      comp_3b;
-    int32_t         comp_3b_enable;
-    comp_prm_t      comp_4b;
-    int32_t         comp_4b_enable;
-    eq_prm_t        eq2;
-    int32_t         eq2_enable;
-    comp_prm_t      limiter;
-    int32_t         limiter_enable;
+    eq_prm_t                eq1;
+    int32_t                 eq1_enable;
+    cross_prm_t             cross;
+    int32_t                 cross_enable;
+    comp_prm_t              comp_1b;
+    int32_t                 comp_1b_enable;
+    comp_prm_t              comp_2b;
+    int32_t                 comp_2b_enable;
+    comp_prm_t              comp_3b;
+    int32_t                 comp_3b_enable;
+    comp_prm_t              comp_4b;
+    int32_t                 comp_4b_enable;
+    eq_prm_t                eq2;
+    int32_t                 eq2_enable;
+    comp_prm_t              limiter;
+    int32_t                 limiter_enable;
 }chain_fxd_prm_t;
 
 typedef struct chain_fxd_states_s{
-    eq_fxd_states_t     eq1;
+    eq_fxd_states_t         eq1;
     cross4b_fxd_states_t    cross;
-    comp_fxd_states_t   comp_1b;
-    comp_fxd_states_t   comp_2b;
-    comp_fxd_states_t   comp_3b;
-    comp_fxd_states_t   comp_4b;
-    eq_fxd_states_t     eq2;
-    comp_fxd_states_t   limiter;
+    comp_fxd_states_t       comp_1b;
+    comp_fxd_states_t       comp_2b;
+    comp_fxd_states_t       comp_3b;
+    comp_fxd_states_t       comp_4b;
+    eq_fxd_states_t         eq2;
+    comp_fxd_states_t       limiter;
 }chain_fxd_states_t;
 
 typedef struct states_s{
-    chain_fxd_states_t Left;
-    chain_fxd_states_t Right;
+    chain_fxd_states_t      Left;
+    chain_fxd_states_t      Right;
 }states_t;
 
 typedef struct chain_fxd_coef_s{
-    eq_fxd_coef_t       eq1;
-    int32_t             eq1_enable;
-    cross_fxd_coef_t    cross[3];
-    int32_t             cross_enable;
-    comp_fxd_coef_t     comp_1b;
-    int32_t             comp_1b_enable;
-    comp_fxd_coef_t     comp_2b;
-    int32_t             comp_2b_enable;
-    comp_fxd_coef_t     comp_3b;
-    int32_t             comp_3b_enable;
-    comp_fxd_coef_t     comp_4b;
-    int32_t             comp_4b_enable;
-    eq_fxd_coef_t       eq2;
-    int32_t             eq2_enable;
-    comp_fxd_coef_t     limiter;
-    int32_t             limiter_enable;
+    eq_fxd_coef_t           eq1;
+    int32_t                 eq1_enable;
+    cross_fxd_coef_t        cross[3];
+    int32_t                 cross_enable;
+    comp_fxd_coef_t         comp_1b;
+    int32_t                 comp_1b_enable;
+    comp_fxd_coef_t         comp_2b;
+    int32_t                 comp_2b_enable;
+    comp_fxd_coef_t         comp_3b;
+    int32_t                 comp_3b_enable;
+    comp_fxd_coef_t         comp_4b;
+    int32_t                 comp_4b_enable;
+    eq_fxd_coef_t           eq2;
+    int32_t                 eq2_enable;
+    comp_fxd_coef_t         limiter;
+    int32_t                 limiter_enable;
 }chain_fxd_coef_t;
 
 #endif
